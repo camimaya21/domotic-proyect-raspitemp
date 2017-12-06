0d3ad860552766fcb12aa33e257bc8a10f19851b
camimaya21 <camilamaya21@gmail.com>
2017-12-06 17:16:54 +0100

    seeds added, loggedin bug fixed

--- api/bin/seeds.js
+++ api/bin/seeds.js
@@ -1,9 +1,35 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const User = require('../models/User');
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+const salt = bcrypt.genSaltSync(bcryptSalt);
 
-{ "username": "cami2",
-  "name": "cami",
-  "email": "cami2@hola.com",
-  "city": "Madrid",
-  "password": "12345678",
-  "lat": "40.392523",
-  "lon": "-3.698436"
+const password = `${process.env.PASSADMIN}`;
+const encryptedPass = bcrypt.hashSync(password, salt);
+
+mongoose.connect(process.env.DBURL)
+
+const user = [
+  { username: "admin",
+  name: "Camila Maya",
+  email: "camilamaya21@gmail.com",
+  city: "Madrid",
+  password: "encryptedPass",
+  lat: "40.392523",
+  lon: "-3.698436",
+  role: "admin"
 }
+
+]
+
+User.create(user, (err, docs) => {
+  if (err) {
+    throw err;
+  }
+
+  docs.forEach((user) => {
+    console.log(user.name)
+  });
+  mongoose.connection.close();
+});
