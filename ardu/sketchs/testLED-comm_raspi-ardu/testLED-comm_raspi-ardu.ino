d8057d1f5ffb185390fa13cac9c057a3d8d1bc76
camimaya21 <camilamaya21@gmail.com>
2017-12-11 18:41:55 +0100

    adding the modified testLED sketch

--- ardu/sketchs/testLED-comm_raspi-ardu/testLED-comm_raspi-ardu.ino
+++ ardu/sketchs/testLED-comm_raspi-ardu/testLED-comm_raspi-ardu.ino
@@ -1,7 +1,7 @@
 void setup() {
 pinMode(LED_BUILTIN, OUTPUT);
 Serial.begin(9600);       //Inicializo el puerto serial a 9600 baudios
-Serial.println("Arduino Ready");
+//Serial.println("Arduino Ready");
 }
 
 void loop() {
@@ -9,10 +9,10 @@ void loop() {
     char c = Serial.read();
     if (c == 'H') {
         digitalWrite(LED_BUILTIN, HIGH);
-        Serial.println("Respuesta de Arduino I'm ON");
+       // Serial.println("Respuesta de Arduino I'm ON");
       }else if (c == 'L'){
           digitalWrite(LED_BUILTIN, LOW);
-          Serial.println("Respuesta de Arduino I'm OFF");
+     //     Serial.println("Respuesta de Arduino I'm OFF");
       }
   }
 }
